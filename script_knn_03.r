b90a88c3d5a399288d945977e65ae8d1ceb3291d
2lsokol2 <36013697+2lsokol2@users.noreply.github.com>
2019-02-12 18:05:46 +0200

    Update script_knn_03.r

--- script_knn_03.r
+++ script_knn_03.r
@@ -1,55 +1,28 @@
-https://coursehunters.net/course/data-science-i-mashinnoe-obuchenie-s-r
 
-
-Ìåòîä ê-ãî áëèæàéøåãî ñîñåäà (knn)
-+ ïðîñòåéøàÿ âàëèäàöè
-
-setwd("F:/scoring/KNN/R_çàíÿòèå_8_1_knn")
 sales <- read.table("DISCRIM_0_new_R.txt", header=T, sep=";")
 
 summary(sales)
 sales[1:5, ]
 
-#  Íå ðåêîìåíäóåòñÿ
-#  head(sales)
-#  tail(sales)
-
-
 dim(sales)
 class(sales[ , 6])
 
 
-#  Ñëó÷àéíàÿ ïîäâûáîðêà
 set.seed(14)
 test.num <- sample(1:nrow(sales), 50, replace = FALSE)
 
-#  Òåñòîâàÿ âûáîðêà
 test <- sales[test.num, 2:5]
 
-#  Îáó÷àþùàÿ âûáîðêà
 train <- sales[-test.num, 2:5]
 
-#  Êîä êëàññà äëÿ îáó÷àþùåé âûáîðêè
 cl <- sales[-test.num, 6]
 
-#  Ïðèñîåäèíÿþ áèáëèîòåêó class
 library(class)
 
-#  Ðàñïîçíàþ êëàññ îáúåêòîâ èç òåñòîâîé âûáîðêè
 zzz1 <- knn(train, test, cl, k = 3)
 
-#  çà÷åì ìîæíî çàäàâàòü prob=TRUE
-
-
-#  Ïðîâåðÿþ ñîîòâåòñòâèå èñòèííîãî è ðàñïîçíàííîãî êëàññîâ
 table(zzz1, sales[test.num, 6])
 
-
-
-#------------------------------
-#  Êàê îïðåäåëÿòü çíà÷åíèå k?
-#  Êðîññ-âàëèäàöèÿ - ïðîñòåéøèé âàðèàíò
-#  ñìîòðè òàêæå ïàêåò caret
 
 a <-rep(0,15)
 
@@ -63,36 +36,59 @@ a
 
 ##testing
 
-#  Ñëó÷àéíàÿ ïîäâûáîðêà
 set.seed(3476)
 test.num <- sample(1:nrow(data), 2000, replace = FALSE)
 
-#  Òåñòîâàÿ âûáîðêà
 test_knn <- data[test.num, 1:12]
 
-#  Îáó÷àþùàÿ âûáîðêà
 train_knn <- data[-test.num, 1:12]
 
-#  Êîä êëàññà äëÿ îáó÷àþùåé âûáîðêè
 cl <- data[-test.num, 1]
 
-#  Ïðèñîåäèíÿþ áèáëèîòåêó class
 library(class)
 
-#  Ðàñïîçíàþ êëàññ îáúåêòîâ èç òåñòîâîé âûáîðêè
 zzz1 <- knn(train_knn, test_knn, cl, k = 3)
 
-#  çà÷åì ìîæíî çàäàâàòü prob=TRUE
-
-
-#  Ïðîâåðÿþ ñîîòâåòñòâèå èñòèííîãî è ðàñïîçíàííîãî êëàññîâ
 table(zzz1, test$target_for_calc)
 
+##########################
+##coutses
+install.packages("ISLR"
+library(ISLR)
+library(dplyr)
+library(class)
+
+previous_data <- as.data.frame(scale(Caravan[,-86]))
+
+# Train Test split
+# choose random observation
+set.seed(3476)
+train.num <- sample(1:nrow(previous_data), round(nrow(previous_data)*.7), replace = FALSE)
+
+test <- previous_data[-train.num, -86]
+test.target <- previous_data[-train.num, 86]
+
+train <- previous_data[train.num, -86]
+train.target <- previous_data[-train.num, 86]
 
 
+# KNN Model
 
+error <- rep(0,15)
+for (i in 1:15) {
+  pred <- knn(train, test, train[,86], k=i)
+  error[i] <- mean(pred != test[,])
+}
 
+error
 
+### VISUALIZE K ELBOW METHOD
 
+k <- 1:15
+error.df <- data.frame(error, k)
 
-
+ggplot(error.df, aes(k, error)) +
+  geom_point() +
+  geom_line(lty='dotted', color='red') +
+  scale_x_continuous(breaks=seq(1, tail(k, n=1),2))
+  
