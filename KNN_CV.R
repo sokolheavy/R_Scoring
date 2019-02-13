3d788fe7e6cc5058d1ecee2fdac78716174a8c9d
2lsokol2 <36013697+2lsokol2@users.noreply.github.com>
2019-02-13 06:56:57 +0200

    Update KNN_CV.R

--- KNN_CV.R
+++ KNN_CV.R
@@ -1,81 +1,129 @@
 
+setwd("F:/scoring/KNN")
+install.packages("ISLR")
+library(ISLR)
+library(dplyr)
+library(class)
+library(DT)
+library(ggplot2)
+                 
+previous_data <- as.data.frame(scale(Caravan[,-86]))
+                 
+# Train Test split
+# choose random observation
+set.seed(3476)
+train.num <- sample(1:nrow(previous_data), round(nrow(previous_data)*.7), replace = FALSE)
+                 
+test <- previous_data[-train.num, -86]
+test.target <- previous_data[-train.num, 86]
+train <- previous_data[train.num, -86]
+train.target <- previous_data[-train.num, 86]
+                 
+                 
+# KNN Model
 
-knn <- function(klist,x.train,y.train,x.test) {
-  # k-nearest neighbors classification
-  # 
-  # klist is a list of values of k to be tested
-  # x.train, y.train: the training set
-  # x.test: the test set
-  # Output: a matrix of predictions for the test set (one column for each k in klist)	
-  # Number of training and test examples
-  n.train <- nrow(x.train)
-  # Matrix to store predictions
-  n.test <- nrow(x.test)
-  
-  p.test <- matrix(NA, n.test, length(klist))
-  
-  # Vector to store the distances of a point to the training points
-  dsq <- numeric(n.train)
-  
-  # Loop on the test instances
-  for (tst in 1:n.test)
-  {
-    # Compute distances to training instances
-    for (trn in 1:n.train)
-    {
-      dsq[trn] <- sum((x.train[trn,] - x.test[tst,])^2)
-    }
-    
-    # Sort distances from smallest to largest
-    ord <- order(dsq)
-    
-    # Make prediction by averaging the k nearest neighbors
-    for (ik in 1:length(klist)) {
-      p.test[tst,ik] <- mean(y.train[ord[1:klist[ik]]])
-    }
-  }
-  
-  # Return the matrix of predictions
-  invisible(p.test)
+error <- rep(0,15)
+for (i in 1:15) {
+pred <- knn(train, test, train[,86], k=i)
+error[i] <- mean(pred != test[,])
+}
+                 
+error
+                 
+### VISUALIZE K ELBOW METHOD
+                 
+k <- 1:15
+error.df <- data.frame(error, k)
+                 
+ggplot(error.df, aes(k, error)) +
+       geom_point() +
+       geom_line(lty='dotted', color='red') +
+                   scale_x_continuous(breaks=seq(1, tail(k, n=1),2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# testing 
+previous_data <- read.table("Wine.txt", header = T)
+sc_data <- as.data.frame(scale(previous_data[,-14]))
+
+datatable(previous_data)
+str(previous_data)
+
+# Train Test split
+# choose random observation
+set.seed(3476)
+train.num <- sample(1:nrow(previous_data), round(nrow(previous_data)*.7), replace = FALSE)
+
+test <- sc_data[-train.num, -14]
+test.target <- previous_data[-train.num, 14]
+train <- sc_data[train.num, -14]
+train.target <- previous_data[train.num, 14]
+
+
+# KNN Model
+
+error <- rep(0,25)
+i <- 1
+for (i in 1:25) {
+  pred <- knn(train, test, train.target, k=i)
+  error[i] <- mean(pred != test.target)
 }
 
+error
 
-knn.cv <- function(klist,x.train,y.train,nfolds) {
-  # Cross-validation for kNN
-  #
+### VISUALIZE K ELBOW METHOD
+
+k <- 1:25
+error.df <- data.frame(error, k)
+
+ggplot(error.df, aes(k, error)) +
+  geom_point() +
+  geom_line(lty='dotted', color='red') +
+  scale_x_continuous(breaks=seq(1, tail(k, n=1),2))
+
+
+x.train <- train
+y.train <- train.target
+nfolds <- 10
+# Cross-validation
+
+knn.cv <- function(x.train,y.train,nfolds) {
   # Perform nfolds-cross validation of kNN, for the values of k in klist
   
   # Number of instances
   n.train <- nrow(x.train)
   
   # Matrix to store predictions
-  p.cv <- matrix(NA, n.train, length(klist))
+  p.cv <- matrix(NA, n.train, seq(n.train))
   
   # Prepare the folds
   s <- split(sample(n.train),rep(1:nfolds,length=n.train))
   
   # Cross-validation
   for (i in seq(nfolds)) {
-    p.cv[s[[i]],] <- knn(klist,x.train[-s[[i]],], y.train[-s[[i]]], x.train[s[[i]],])
+    p.cv[s[[i]],] <- knn(klist,x.train[-s[[i]],], y.train[-s[[i]]], 9)
   }
   
   # Return matrix of CV predictions
   invisible(p.cv)
 }
 
-
-
-# Download prostate data
-con = url ("http://www-stat.stanford.edu/~tibs/ElemStatLearn/datasets/prostate.data")
-prost=read.csv(con,row.names=1,sep="\t")
-# Alternatively, load the file and read from local file as follows
-# prost=read.csv('prostate.data.txt',row.names=1,sep="\t")
-
-# Create training and test sets
-x.train <- prost[prost$train,1:8]
-x.test <- prost[!prost$train,1:8]
-y.train <- prost[prost$train,9]
-y.test <- prost[!prost$train,9]
+knn.cv(train, train.target, 10)
 
 # Make predictions by kNN
 klist <- seq(n.train) # we test all values of k
@@ -94,4 +142,3 @@ plot(mse.train , ylim=c(0,2) , type='l' , xlab='k' , ylab='MSE', col=1 , lwd=2)
 lines(mse.test , col=2 , lwd=2)
 lines(mse.cv, col=3 , lwd=2)
 legend("bottomright",legend=c('Train','Test','CV'),text.col=seq(3) , lty=1 , col=seq(3))
-
