ebe3db7e9500be6237b27495829086091094e570
Sokol Olena <36013697+Sokolheavy@users.noreply.github.com>
2019-03-29 06:51:16 +0200

    Update Data Hackathon.R

--- GBM/Tune_parameters_cases/Data Hackathon.R
+++ GBM/Tune_parameters_cases/Data Hackathon.R
@@ -1,1 +1,9 @@
-library(xgboom)
+# https://www.analyticsvidhya.com/blog/2016/02/complete-guide-parameter-tuning-gradient-boosting-gbm-python/
+
+library(dplyr)
+library(data.table)
+library(xgboost)
+
+train = fread("https://raw.githubusercontent.com/Sokolheavy/R_Scoring/master/GBM/Tune_parameters_cases/train_modified.csv")
+target = 'Disbursed'
+IDcol = 'ID'
