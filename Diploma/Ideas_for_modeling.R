94de7235c5463da0561841854f98a8a1950dcd3e
Sokol Olena <36013697+Sokolheavy@users.noreply.github.com>
2019-04-02 07:38:46 +0300

    Update and rename CV.R to Ideas_for_modeling.R

---
+++ Diploma/Ideas_for_modeling.R
@@ -0,0 +1,10 @@
+https://github.com/ShuaiW/kaggle-classification   --all topics for classification
+
+
+https://www.kaggle.com/jatinder8889/classification-end-to-end-project-in-r
+https://www.kaggle.com/c/otto-group-product-classification-challenge/discussion/13585
+https://www.kaggle.com/niteshx2/top-50-beginners-stacking-lgb-xgb
+https://machinelearningmastery.com/machine-learning-in-r-step-by-step/
+https://www.kaggle.com/yibochen/xgboost-in-r-2-27217 --xgboost
+https://www.kaggle.com/grosvenpaul/beginners-guide-to-eda-and-random-forest-using-r --RF
+https://www.kaggle.com/akshaydighe31/binary-classification-using-random-forest
