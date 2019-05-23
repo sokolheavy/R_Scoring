833735f50a6ec157973692122792094f98afaa51
Sokol Olena <36013697+Sokolheavy@users.noreply.github.com>
2019-05-23 18:33:51 +0300

    Update Preprocessing.R

--- Preprocessing/Preprocessing_Classification/Preprocessing.R
+++ Preprocessing/Preprocessing_Classification/Preprocessing.R
@@ -41,3 +41,55 @@ suppressMessages(library('corrplot'))
 suppressMessages(library('plyr'))
 
 #Loading Data
+
+train <- read.csv('https://raw.githubusercontent.com/Sokolheavy/R_Scoring/master/Preprocessing/Preprocessing_Classification/train.csv', stringsAsFactors = F)
+test  <- read.csv('https://raw.githubusercontent.com/Sokolheavy/R_Scoring/master/Preprocessing/Preprocessing_Classification/test.csv', stringsAsFactors = F)
+
+full  <- bind_rows(train, test) # test + train
+
+options( warn = -1 )
+
+str(full)
+
+summary(full)
+# dev.off
+
+
+
+#______________________________________________________________________________
+#_____________________________Exploratory Analysis_____________________________
+#______________________________________________________________________________
+
+## 2.1
+
+# Age vs Survived
+ggplot(full[1:891,], aes(Age, fill = factor(Survived))) + 
+  geom_histogram(bins=30) + 
+  theme_few() +
+  xlab("Age") +
+  scale_fill_discrete(name = "Survived") + 
+  ggtitle("Age vs Survived")
+
+## 2.2
+
+# Sex vs Survived
+ggplot(full[1:891,], aes(Sex, fill = factor(Survived))) + 
+  geom_bar(stat = "count", position = 'dodge')+
+  theme_few() +
+  xlab("Sex") +
+  ylab("Count") +
+  scale_fill_discrete(name = "Survived") + 
+  ggtitle("Sex vs Survived")
+
+## 2.3. Age vs Sex
+
+#Sex vs Survived vs Age 
+ggplot(full[1:891,], aes(Age, fill = factor(Survived))) + 
+  geom_histogram(bins=30) + 
+  theme_few() +
+  xlab("Age") +
+  ylab("Count") +
+  facet_grid(.~Sex)+
+  scale_fill_discrete(name = "Survived") + 
+  theme_few()+
+  ggtitle("Age vs Sex vs Survived")
